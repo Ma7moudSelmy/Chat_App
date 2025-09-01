f89b6b2889be70cc3eaf93560c8514fedc18d250
Mahmoudselmy <Ma7moud.m.selmy1@gmail.com>
2025-09-01 12:17:32 +0300

    Integrate Firebase into Flutter application with necessary configurations and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:chat_app/Screens/login_page.dart';
+import 'package:chat_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   runApp(chat_app());
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
 
 class chat_app extends StatelessWidget {
